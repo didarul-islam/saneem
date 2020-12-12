708da6d0865f36eb9e01cd83cd8a8d0ecba561bc
Didarul Islam <71816650+didarul-islam@users.noreply.github.com>
2020-12-13 03:41:30 +0600

    second edit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 
+// $(document).ready(function(){
 
-    var mixer = mixitup('.container');
+//     $(".js--services-section").waypoint(function(direction){
+//         if(direction=="down"){
+//             $("nav").addClass("sticky")
+//         }else{
+//             $("nav").removeClass("sticky");
+//         }
+//     });
+    
+        var mixer = mixitup(".container");
+// });
 
+
+
+
+window.onscroll = function() {myFunction()};
+
+var mainNav = document.getElementById("main-nav");
+var sticky = mainNav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    mainNav.classList.add("sticky");
+  } else {
+    mainNav.classList.remove("sticky");
+  }
+}
+
+
+function openNav(){
+  document.getElementById('myNav').style.width='100%'
+}
+function closeNav(){
+  document.getElementById('myNav').style.width='0%'
+}
